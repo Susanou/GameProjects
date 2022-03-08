ae7afd7eeaa2ad738015b77d644a6de27a7e5d8d
Fukurou <caos2804@gmail.com>
2022-03-08 00:23:43 -0500

    Added jumping to character

--- CoopGame/Source/CoopGame/Public/SCharacter.h
+++ CoopGame/Source/CoopGame/Public/SCharacter.h
@@ -41,4 +41,8 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+//challenge code
+protected:
+	
+	void OnJump();
 };
